945886c28f126b2dd7d13d01963746c7ef3fac9e
jneilliii <jneilliii+github@gmail.com>
2020-07-04 00:28:46 -0400

    0.0.3
    Enable loading/unloading based on tab visibility and window minimized state.

--- octoprint_webcam_iframe/static/js/webcam_iframe.js
+++ octoprint_webcam_iframe/static/js/webcam_iframe.js
@@ -9,16 +9,35 @@ $(function() {
 		var self = this;
 
 		self.settingsViewModel = parameters[0];
+		self.controlViewModel = parameters[1];
 
 		self.onAllBound = function(allViewModels){
-			var webcamurl = self.settingsViewModel.webcam_streamUrl();
-			$('#webcam_container').replaceWith('<iframe id="webcam_container" src="' + webcamurl + '" width="588" height="330" style="border: none;"></iframe>');
+			$('#webcam_container').replaceWith('<iframe id="webcam_container" src="abount:blank" width="588" height="330" style="border: none;"></iframe>');
 		}
+
+		self.onTabChange = function(current, previous) {
+				if (current === "#control") {
+					$('#webcam_container').attr("src", self.settingsViewModel.webcam_streamUrl());
+				}
+				if (previous === "#control") {
+					$('#webcam_container').attr("src", "about:blank");
+				}
+			};
+
+		self.controlViewModel.onBrowserTabVisibilityChange = function(status) {
+			if (status) {
+				self.controlViewModel._enableWebcam();
+				$('#webcam_container').attr("src", self.settingsViewModel.webcam_streamUrl());
+			} else {
+				$('#webcam_container').attr("src", "about:blank");
+				self.controlViewModel._disableWebcam();
+			}
+		};
 	}
 
 	OCTOPRINT_VIEWMODELS.push({
 		construct: Webcam_iframeViewModel,
-		dependencies: ["settingsViewModel"],
+		dependencies: ["settingsViewModel", "controlViewModel"],
 		elements: [ /* ... */ ]
 	});
 });
