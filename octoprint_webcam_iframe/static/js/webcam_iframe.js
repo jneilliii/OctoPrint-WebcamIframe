1165ee53209ebb5132c439be81fd20cb825e94b4
Clonesht <62205813+clonesht@users.noreply.github.com>
2021-01-26 23:03:57 +0100

    Support iframe webcam in dashboard plugin
    https://plugins.octoprint.org/plugins/dashboard/

--- octoprint_webcam_iframe/static/js/webcam_iframe.js
+++ octoprint_webcam_iframe/static/js/webcam_iframe.js
@@ -22,6 +22,20 @@ $(function() {
 				if (previous === "#control") {
 					$('#webcam_container').attr("src", "about:blank");
 				}
+            
+				// Dashboard plugin
+				if (current === "#tab_plugin_dashboard") {
+					var dashboard_webcam = $('#plugin_dashboard_webcam_container');
+					if (dashboard_webcam.length == 0) {  // We have to do it here, because dashboard_webcam_image object not exists yet when onAllBound triggered
+						dashboard_webcam = $('<iframe id="plugin_dashboard_webcam_container" src="about:blank" width="100%" height="100%" style="border: none;" allow="autoplay *; fullscreen *"></iframe>');
+						$("#dashboard_webcam_image").replaceWith(dashboard_webcam);
+					}
+
+					dashboard_webcam.attr("src", self.settingsViewModel.webcam_streamUrl());
+				}
+				if (previous === "#tab_plugin_dashboard") {
+					$('#plugin_dashboard_webcam_container').attr("src", "about:blank");
+				}
 			};
 
 		self.controlViewModel.onBrowserTabVisibilityChange = function(status) {
